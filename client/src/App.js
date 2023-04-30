b40fd17b36be3f84611cd87f90a4311f25a6c094
Petar Mirchev <petar.g.mirchev@gmail.com>
2023-04-30 12:10:11 +0300

    finish Login form design
    finish Login form design

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Profile from './pages/profile/Profile';
+import Register from './pages/register/Register';
 
 function App() {
   return (
     <div className="App">
-      <h1> Hello </h1>
+
+      {/* <Home/> */}
+      <Login/>
+      {/* <Profile/> */}
+      {/* <Register/> */}
     </div>
   );
 }
--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -3,7 +3,27 @@ import './login.scss';
 
 const Login = () => {
   return (
-    <div className='login'>Login</div>
+    <div className='login'>
+      <div className="card">
+        <div className="left">
+          <h1>Hello User.</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae aut enim, 
+            accusantium veniam dolores provident dolor minus doloribus vel cupiditate 
+            velit modi temporibus ipsam consequuntur quo quis atque dolore? Blanditiis.
+          </p>
+          <span>Don't you have an account?</span>
+          <button>Register</button>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input type='text' placeholder='username'/>
+            <input type='password' placeholder='password'/>
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
