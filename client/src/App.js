676d10e7e4c9df1607fba6c637c659b5901560a0
Petar Mirchev <petar.g.mirchev@gmail.com>
2023-04-30 13:07:43 +0300

    install & set react-router-dom
     set react-router-dom + error page

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,52 @@
 import './App.css';
+import ErrorPage from './pages/Error/ErrorPage';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import Profile from './pages/profile/Profile';
 import Register from './pages/register/Register';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+
 
 function App() {
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login/>,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/profile",
+    element: <Profile/>,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/",
+    element: <Home/>,
+    errorElement: <ErrorPage />,
+  },
+]);
+
+
+
   return (
     <div className="App">
+      <RouterProvider router={router} />
 
       {/* <Home/> */}
       {/* <Login/> */}
       {/* <Profile/> */}
-      <Register/>
+      {/* <Register/> */}
     </div>
   );
 }
