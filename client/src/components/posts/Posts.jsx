5d55cf0e234700374374054f9061c91b34eddd6f
Petar Mirchev <petar.g.mirchev@gmail.com>
2023-05-05 23:24:05 +0300

    create post design
    create post  design + style

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -1,10 +1,47 @@
 import React from 'react';
 import './posts.scss';
+import Post from '../post/Post';
 
 const Posts = () => {
+
+  //temp data
+  const posts = [
+  {
+    id: 1,
+    name: "Random Name",
+    userId: 1,
+    profilePic:
+    "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cG9ydHJhaXR8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60",
+    desc: "Lorem ipsum dolor sit amet, consectetur",
+    img: "https://images.unsplash.com/photo-1519417688547-61e5d5338ab0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cG9zdHN8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60",
+  },
+  {
+    id: 2,
+    name: "Random Name",
+    userId: 2,
+    profilePic:
+    "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cG9ydHJhaXR8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60",
+    desc: "Lorem ipsum dolor sit amet, consectetur",
+    img: "https://images.unsplash.com/photo-1592762224962-5f98a3a00621?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHBvc3RzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
+  },
+  {
+    id: 3,
+    name: "Random Name",
+    userId: 3,
+    profilePic:
+    "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cG9ydHJhaXR8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60",
+    desc: "Lorem ipsum dolor sit amet, consectetur",
+    img: "https://images.unsplash.com/photo-1585416294584-b849d9e571ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8cG9zdHN8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60",
+  },
+  ];
+
+
+
   return (
     <div className='posts'>
-        Posts
+        {posts.map( post => (
+          <Post post={post} key={post.id}/>
+        ))}
     </div>
   )
 }
--- client/src/components/stories/Stories.jsx
+++ client/src/components/stories/Stories.jsx
@@ -10,22 +10,22 @@ const Stories = () => {
     const stories = [
         {
           id: 1,
-          name: "John Doe",
+          name: "Gogo",
           img: "https://images.unsplash.com/photo-1626236104436-1e5aa5dd6792?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8c3Rvcnl8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60",
         },
         {
           id: 2,
-          name: "John Doe",
+          name: "Pepi",
           img: "https://images.unsplash.com/photo-1578852612716-854e527abf2e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8c3Rvcnl8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60",
         },
         {
           id: 3,
-          name: "John Doe",
+          name: "Ani",
           img: "https://plus.unsplash.com/premium_photo-1664046912344-df11b4ae17ed?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8c3Rvcnl8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60",
         },
         {
           id: 4,
-          name: "John Doe",
+          name: "Mimi",
           img: "https://plus.unsplash.com/premium_photo-1664100478033-7f71a67363d0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fHN0b3J5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
         },
       ];
